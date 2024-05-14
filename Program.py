5dac1bf732d825fbc35a891adb2e32581eb30de4
akulov1 <zhibzhi@mail.ru>
2024-05-14 23:41:07 +0300

    add Bogdanchik

--- Program.py
+++ Program.py
@@ -4,6 +4,7 @@ from random import uniform
 # randint(начало, конец) - возрщает рандомное целое в диапазоне включая концы
 
 import Akulov_task
+import Kim_tasks
 import sys
 import os
 
@@ -19,7 +20,7 @@ def clicked():
     else:
         variants_count = 1
     if chk_all.get():
-        for i in range(1, 21):
+        for i in range(1, 22):
             tasks_num_list.append(i)
     else:
         number = 1
@@ -62,7 +63,7 @@ for i in range(0, 21):
         task_check = Checkbutton(window, text=f'0{i + 1}', var=check_list[i], width=10)
     else:
         task_check = Checkbutton(window, text=f'{i + 1}', var=check_list[i], width=10)
-    task_check.grid(column=0, row=i + 3, )
+    task_check.grid(column=0, row=i + 3)
 btn = Button(window, text='Создать документ', bg='orange', fg='black', command=clicked)
 btn.grid(row=27)
 
@@ -77,11 +78,11 @@ for i in range(0, variants_count):
     for task in tasks_num_list:  # идем по списку с номерами заданий
         if task == 1:  # если номер = 1, то пишем его
             #coin_flip = randint(1, 3)  # 1 - вариант Егора, 2 - Маги, 3 - Димы
-            coin_flip = 1
+            coin_flip = 2
             if coin_flip == 1:  # задание Егора
                 text, answer = Akulov_task.task_1()
-            #elif coin_flip == 2:
-            #    text, answer = Magas_tasks.task_1()
+            elif coin_flip == 2:
+                text, answer = Kim_tasks.task_1()
             #else:
             #    text, answer = Dima_tasks.task_1()
             tasks.write(text+'\n')
@@ -89,12 +90,12 @@ for i in range(0, variants_count):
             continue
 
         if task == 2:  # если номер = 2, то пишем 2 и т.д.
-            coin_flip = 1  # 1 - вариант Егора, 2 - Маги, 3 - Димы
+            coin_flip = 2  # 1 - вариант Егора, 2 - Маги, 3 - Димы
             if coin_flip == 1:  # задание Егора
                 text, answer = Akulov_task.task_2()
-            # 
-            # elif coin_flip == 2:
-            #     text, answer = Magas_tasks.task_2()
+             
+            elif coin_flip == 2:
+                text, answer = Kim_tasks.task_2()
             # else:
             #     text, answer = Dima_tasks.task_2()
             tasks.write(text+'\n')
@@ -102,12 +103,12 @@ for i in range(0, variants_count):
             continue
 
         if task == 3:
-            coin_flip = 1  # 1 - вариант Егора, 2 - Маги, 3 - Димы
+            coin_flip = 2  # 1 - вариант Егора, 2 - Маги, 3 - Димы
             if coin_flip == 1:  # задание Егора
                 text, answer = Akulov_task.task_3()
 
-            # elif coin_flip == 2:
-            #     text, answer = Magas_tasks.task_3()
+            elif coin_flip == 2:
+                text, answer = Kim_tasks.task_3()
             # else:
             #     text, answer = Dima_tasks.task_3()
             tasks.write(text+'\n')
@@ -115,12 +116,12 @@ for i in range(0, variants_count):
             continue
 
         if task == 4:
-            coin_flip = 1  # 1 - вариант Егора, 2 - Маги, 3 - Димы
+            coin_flip = 2  # 1 - вариант Егора, 2 - Маги, 3 - Димы
             if coin_flip == 1:  # задание Егора
                 text, answer = Akulov_task.task_4()
 
-            # elif coin_flip == 2:
-            #     text, answer = Magas_tasks.task_4()
+            elif coin_flip == 2:
+                text, answer = Kim_tasks.task_4()
             # else:
             #     text, answer = Dima_tasks.task_4()
             tasks.write(text+'\n')
@@ -128,12 +129,12 @@ for i in range(0, variants_count):
             continue
 
         if task == 5:
-            coin_flip = 1# 1 - вариант Егора, 2 - Маги, 3 - Димы
+            coin_flip = 2# 1 - вариант Егора, 2 - Маги, 3 - Димы
             if coin_flip == 1:  # задание Егора
                 pass
 
-            # elif coin_flip == 2:
-            #     text, answer = Magas_tasks.task_5()
+            elif coin_flip == 2:
+                text, answer = Kim_tasks.task_5()
             # else:
             #     text, answer = Dima_tasks.task_5()
             tasks.write(text+'\n')
@@ -141,12 +142,12 @@ for i in range(0, variants_count):
             continue
 
         if task == 6:
-            coin_flip = 1# 1 - вариант Егора, 2 - Маги, 3 - Димы
+            coin_flip = 2# 1 - вариант Егора, 2 - Маги, 3 - Димы
             if coin_flip == 1:  # задание Егора
                 text, answer = Akulov_task.task_6()
 
-            # elif coin_flip == 2:
-            #     text, answer = Magas_tasks.task_6()
+            elif coin_flip == 2:
+                text, answer = Kim_tasks.task_6()
             # else:
             #     text, answer = Dima_tasks.task_6()
             tasks.write(text+'\n')
@@ -154,12 +155,12 @@ for i in range(0, variants_count):
             continue
 
         if task == 7:
-            coin_flip = 1  # 1 - вариант Егора, 2 - Маги, 3 - Димы
+            coin_flip = 2  # 1 - вариант Егора, 2 - Маги, 3 - Димы
             if coin_flip == 1:  # задание Егора
                 text, answer = Akulov_task.task_7()
-            #
-            # elif coin_flip == 2:
-            #     text, answer = Magas_tasks.task_7()
+
+            elif coin_flip == 2:
+                text, answer = Kim_tasks.task_7()
             # else:
             #     text, answer = Dima_tasks.task_7()
             tasks.write(text+'\n')
@@ -167,12 +168,12 @@ for i in range(0, variants_count):
             continue
 
         if task == 8:
-            coin_flip = 1  # 1 - вариант Егора, 2 - Маги, 3 - Димы
+            coin_flip = 2  # 1 - вариант Егора, 2 - Маги, 3 - Димы
             if coin_flip == 1:  # задание Егора
                 text, answer = Akulov_task.task_8()
-            #
-            # elif coin_flip == 2:
-            #     text, answer = Magas_tasks.task_8()
+
+            elif coin_flip == 2:
+                text, answer = Kim_tasks.task_8()
             # else:
             #     text, answer = Dima_tasks.task_8()
             tasks.write(text+'\n')
@@ -180,12 +181,12 @@ for i in range(0, variants_count):
             continue
 
         if task == 9:
-            coin_flip = 1  # 1 - вариант Егора, 2 - Маги, 3 - Димы
+            coin_flip = 2  # 1 - вариант Егора, 2 - Маги, 3 - Димы
             if coin_flip == 1:  # задание Егора
                 text, answer = Akulov_task.task_9()
 
-            # elif coin_flip == 2:
-            #     text, answer = Magas_tasks.task_9()
+            elif coin_flip == 2:
+                text, answer = Kim_tasks.task_9()
             # else:
             #     text, answer = Dima_tasks.task_9()
             tasks.write(text+'\n')
@@ -193,12 +194,12 @@ for i in range(0, variants_count):
             continue
 
         if task == 10:
-            coin_flip = 1  # 1 - вариант Егора, 2 - Маги, 3 - Димы
+            coin_flip = 2  # 1 - вариант Егора, 2 - Маги, 3 - Димы
             if coin_flip == 1:  # задание Егора
                 text, answer = Akulov_task.task_10()
 
-            # elif coin_flip == 2:
-            #     text, answer = Magas_tasks.task_10()
+            elif coin_flip == 2:
+                text, answer = Kim_tasks.task_10()
             # else:
             #     text, answer = Dima_tasks.task_10()
             tasks.write(text+'\n')
@@ -206,12 +207,12 @@ for i in range(0, variants_count):
             continue
 
         if task == 11:
-            coin_flip = 1  # 1 - вариант Егора, 2 - Маги, 3 - Димы
+            coin_flip = 2  # 1 - вариант Егора, 2 - Маги, 3 - Димы
             if coin_flip == 1:  # задание Егора
                 text, answer = Akulov_task.task_11()
-            #
-            # elif coin_flip == 2:
-            #     text, answer = Magas_tasks.task_11()
+
+            elif coin_flip == 2:
+                text, answer = Kim_tasks.task_11()
             # else:
             #     text, answer = Dima_tasks.task_11()
             tasks.write(text+'\n')
@@ -219,12 +220,12 @@ for i in range(0, variants_count):
             continue
 
         if task == 12:
-            coin_flip = 1  # 1 - вариант Егора, 2 - Маги, 3 - Димы
+            coin_flip = 2  # 1 - вариант Егора, 2 - Маги, 3 - Димы
             if coin_flip == 1:  # задание Егора
                 text, answer = Akulov_task.task_12()
-            #
-            # elif coin_flip == 2:
-            #     text, answer = Magas_tasks.task_12()
+
+            elif coin_flip == 2:
+                text, answer = Kim_tasks.task_12()
             # else:
             #     text, answer = Dima_tasks.task_12()
             tasks.write(text+'\n')
@@ -232,12 +233,12 @@ for i in range(0, variants_count):
             continue
 
         if task == 13:
-            coin_flip = 1  # 1 - вариант Егора, 2 - Маги, 3 - Димы
+            coin_flip = 2  # 1 - вариант Егора, 2 - Маги, 3 - Димы
             if coin_flip == 1:  # задание Егора
                 text, answer = Akulov_task.task_13()
 
-            # elif coin_flip == 2:
-            #     text, answer = Magas_tasks.task_13()
+            elif coin_flip == 2:
+                text, answer = Kim_tasks.task_13()
             # else:
             #     text, answer = Dima_tasks.task_13()
             tasks.write(text+'\n')
@@ -245,12 +246,12 @@ for i in range(0, variants_count):
             continue
 
         if task == 14:
-            coin_flip = 1  # 1 - вариант Егора, 2 - Маги, 3 - Димы
+            coin_flip = 2  # 1 - вариант Егора, 2 - Маги, 3 - Димы
             if coin_flip == 1:  # задание Егора
                 text, answer = Akulov_task.task_14()
-            #
-            # elif coin_flip == 2:
-            #     text, answer = Magas_tasks.task_14()
+
+            elif coin_flip == 2:
+                text, answer = Kim_tasks.task_14()
             # else:
             #     text, answer = Dima_tasks.task_14()
             tasks.write(text+'\n')
@@ -258,12 +259,12 @@ for i in range(0, variants_count):
             continue
 
         if task == 15:
-            coin_flip = 1  # 1 - вариант Егора, 2 - Маги, 3 - Димы
+            coin_flip = 2  # 1 - вариант Егора, 2 - Маги, 3 - Димы
             if coin_flip == 1:  # задание Егора
                 text, answer = Akulov_task.task_15()
 
-            # elif coin_flip == 2:
-            #     text, answer = Magas_tasks.task_15()
+            elif coin_flip == 2:
+                text, answer = Kim_tasks.task_15()
             # else:
             #     text, answer = Dima_tasks.task_15()
             tasks.write(text+'\n')
@@ -271,12 +272,11 @@ for i in range(0, variants_count):
             continue
 
         if task == 16:
-            coin_flip = 1  # 1 - вариант Егора, 2 - Маги, 3 - Димы
+            coin_flip = 2  # 1 - вариант Егора, 2 - Маги, 3 - Димы
             if coin_flip == 1:  # задание Егора
                 text, answer = Akulov_task.task_16()
-            #
-            # elif coin_flip == 2:
-            #     text, answer = Magas_tasks.task_16()
+            elif coin_flip == 2:
+                text, answer = Kim_tasks.task_16()
             # else:
             #     text, answer = Dima_tasks.task_16()
             tasks.write(text+'\n')
@@ -284,12 +284,12 @@ for i in range(0, variants_count):
             continue
 
         if task == 17:
-            coin_flip = 1  # 1 - вариант Егора, 2 - Маги, 3 - Димы
+            coin_flip = 2  # 1 - вариант Егора, 2 - Маги, 3 - Димы
             if coin_flip == 1:  # задание Егора
                 text, answer = Akulov_task.task_17()
-            #
-            # elif coin_flip == 2:
-            #     text, answer = Magas_tasks.task_17()
+
+            elif coin_flip == 2:
+                text, answer = Kim_tasks.task_17()
             # else:
             #     text, answer = Dima_tasks.task_17()
             tasks.write(text+'\n')
@@ -297,12 +297,12 @@ for i in range(0, variants_count):
             continue
 
         if task == 18:
-            coin_flip = 1 # 1 - вариант Егора, 2 - Маги, 3 - Димы
+            coin_flip = 2 # 1 - вариант Егора, 2 - Маги, 3 - Димы
             if coin_flip == 1:  # задание Егора
                 text, answer = Akulov_task.task_18()
 
-            # elif coin_flip == 2:
-            #     text, answer = Magas_tasks.task_18()
+            elif coin_flip == 2:
+                text, answer = Kim_tasks.task_18()
             # else:
             #     text, answer = Dima_tasks.task_18()
             tasks.write(text+'\n')
@@ -310,12 +310,12 @@ for i in range(0, variants_count):
             continue
 
         if task == 19:
-            coin_flip = 1  # 1 - вариант Егора, 2 - Маги, 3 - Димы
+            coin_flip = 2  # 1 - вариант Егора, 2 - Маги, 3 - Димы
             if coin_flip == 1:  # задание Егора
                 text, answer = Akulov_task.task_19()
-            #
-            # elif coin_flip == 2:
-            #     text, answer = Magas_tasks.task_19()
+
+            elif coin_flip == 2:
+                text, answer = Kim_tasks.task_19()
             # else:
             #     text, answer = Dima_tasks.task_19()
             tasks.write(text+'\n')
@@ -323,28 +323,25 @@ for i in range(0, variants_count):
             continue
 
         if task == 20:
-            coin_flip = 1  # 1 - вариант Егора, 2 - Маги, 3 - Димы
+            coin_flip = 2  # 1 - вариант Егора, 2 - Маги, 3 - Димы
             if coin_flip == 1:  # задание Егора
                 text, answer = Akulov_task.task_20()
 
-            # elif coin_flip == 2:
-            #     text, answer = Magas_tasks.task_20()
+            elif coin_flip == 2:
+                text, answer = Kim_tasks.task_20()
             # else:
             #     text, answer = Dima_tasks.task_20()
             tasks.write(text+'\n')
             answers.write(answer+'\n')
             continue
         if task == 21:
-            coin_flip = 1  # 1 - вариант Егора, 2 - Маги, 3 - Димы
+            coin_flip = 1
             if coin_flip == 1:
                 text, answer = Akulov_task.task_21()
-                # задание Егора
             elif coin_flip == 2:
-                pass
-                # задание Маги
-            else:
-                pass
-                # задание Димы
+                print(1)
+                text, answer = Kim_tasks.task_21()
+                print(2)
             tasks.write(text + '\n')
             answers.write(answer + '\n')
             continue
